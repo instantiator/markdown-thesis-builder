d04e18b2d27aeb888067584e397de8f200461112
Lewis Westbury <lewis@cantab.net>
2021-06-17 20:42:02 +0100

    Improved screenshots, and util script

---
+++ utils/pdf-to-pngs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+brew install ghostscript
+gs -dNOPAUSE -sDEVICE=png16m -r256 -sOutputFile=page%03d.png $1
+
+for FILENAME in `ls page*.png | sort`; do
+  convert $FILENAME -bordercolor white -border 13 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage $FILENAME.shadow.png
+
+done
